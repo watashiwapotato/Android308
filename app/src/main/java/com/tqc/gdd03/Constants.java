ebdf21f4b9be889c4fba07b1bb1ac667f5557ab7
watashiwapotato <matt104072@gmail.com>
2020-03-13 19:11:15 +0800

    1.0.0

---
+++ app/src/main/java/com/tqc/gdd03/Constants.java
@@ -0,0 +1,12 @@
+package com.tqc.gdd03;
+
+/**
+ * Created by Hippo on 2018
+ */
+public class Constants
+{
+  public static final int EXTRA_WRITE_STORAGE = 0x100001;
+  public static final int MobileData = 2;
+  public static final int WifiData = 1;
+  public static final String EXTRA_KEY_INDEX = "EXTRA_KEY_INDEX";
+}
